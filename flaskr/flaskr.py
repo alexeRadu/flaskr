aa99fb6cffe537ee28f72698947597c0d2f978ef
Alexe Radu Andrei <alexeRadu2007@gmail.com>
2017-10-12 18:44:07 +0300

    add a small stub as a module
    Signed-off-by: Alexe Radu Andrei <alexeRadu2007@gmail.com>

---
+++ flaskr/flaskr.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+NAME = 'radu'
+
+# create the application instance
+app = Flask(__name__)
+
+# load config from this file
+app.config.from_object(__name__)
+
+@app.route('/')
+def hello_world():
+	return "Hello there"
