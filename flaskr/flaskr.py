168effe030134d5579ea02a4d7aff4823ab33735
Alexe Radu Andrei <alexeRadu2007@gmail.com>
2017-10-13 14:45:47 +0300

    add support for console printing
    This is in case I need console debugging.
    
    Signed-off-by: Alexe Radu Andrei <alexeRadu2007@gmail.com>

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -1,4 +1,10 @@
+from __future__ import print_function
+import sys
 from flask import Flask
+
+# print to console
+def cprint(msg):
+	print(msg, file=sys.stderr)
 
 # create the application instance
 app = Flask(__name__)
@@ -8,4 +14,5 @@ app.config.from_object(__name__)
 
 @app.route('/')
 def hello_world():
+	cprint("Console messsage from %s" % ("Radu"))
 	return "Hello there"
