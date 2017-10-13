33f415b9faf66a98ff17c083b4832098c95018b3
Alexe Radu Andrei <alexeRadu2007@gmail.com>
2017-10-13 15:03:34 +0300

    update configuration options
    Signed-off-by: Alexe Radu Andrei <alexeRadu2007@gmail.com>

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-import sys
+import sys, os
 from flask import Flask
 
 # print to console
@@ -16,6 +16,19 @@ app = Flask(__name__)
 # For a larger application the default configuration could be stored into either
 # a config.py or a config.ini file and loaded at this point.
 app.config.from_object(__name__)
+
+# Manually override default config
+app.config.update(dict(
+	DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+	SECRET_KEY="development key",
+	USERNAME="admin",
+	PASSWORD="default"
+))
+
+# Override config from environmental parameters. The variable FLASKR_SETTINGS
+# should point to the configuration file.
+# ex: export FLASKR_SETTINGS=config.py
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 @app.route('/')
 def hello_world():
