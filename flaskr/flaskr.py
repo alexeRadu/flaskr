3590cc62adc0b5ff23b135b55f64a5d4dfc494ea
Alexe Radu Andrei <alexeRadu2007@gmail.com>
2017-10-13 16:41:59 +0300

    add posibility to entry message
    Signed-off-by: Alexe Radu Andrei <alexeRadu2007@gmail.com>

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -67,6 +67,18 @@ def show_entries():
 	entries = query_db('select title, text from entries order by id desc')
 	return render_template('show_entries.html', entries=entries)
 
+@app.route('/add', methods=['POST'])
+def add_entry():
+	if not session.get('logged_in'):
+		abort(401)
+	db = get_db()
+	db.execute('insert into entries (title, text) values (?, ?)',
+		[request.form['title'], request.form['text']])
+	db.commit()
+	flash('New entry was successfully posted')
+
+	return redirect(url_for('show_entries'))
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	error = None
